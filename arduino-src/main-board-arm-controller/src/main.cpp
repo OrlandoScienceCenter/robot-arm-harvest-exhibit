9c2584c0c3a4e9f9c7ab4f75d12481cec779d841
DSikes <pockybum522@gmail.com>
2024-06-06 16:58:12 -0400

    All working from joysticks to ESP-32 and everything in between

--- arduino-src/main-board-arm-controller/src/main.cpp
+++ arduino-src/main-board-arm-controller/src/main.cpp
@@ -51,20 +51,68 @@ void loop()
         Serial.print("incoming line was: ");
         Serial.println(incomingData.c_str());
 
+        // Left stick
         if (incomingData[0] == '1')
         {
-            Serial.println("Moving elbow UP!");
-            moveElbowUp();
+            Serial.println("Left stick move: UP!");
         }
 
         if (incomingData[1] == '1')
         {
-            Serial.println("Moving elbow DOWN!");
-            moveElbowDown();
+            Serial.println("Left stick move: RIGHT!");
+        }
+
+        if (incomingData[2] == '1')
+        {
+            Serial.println("Left stick move: DOWN!");
+        }
+
+        if (incomingData[3] == '1')
+        {
+            Serial.println("Left stick move: LEFT!");
+        }
+
+        if (incomingData[4] == '1')
+        {
+            Serial.println("Left stick: BUTTON!");
+        }
+
+        // Right stick
+        if (incomingData[5] == '1')
+        {
+            Serial.println("Right stick move: UP!");
+        }
+
+        if (incomingData[6] == '1')
+        {
+            Serial.println("Right stick move: RIGHT!");
+        }
+
+        if (incomingData[7] == '1')
+        {
+            Serial.println("Right stick move: DOWN!");
+        }
+
+        if (incomingData[8] == '1')
+        {
+            Serial.println("Right stick move: LEFT!");
+        }
+
+        if (incomingData[9] == '1')
+        {
+            Serial.println("Right stick: BUTTON!");
         }
 
         if (incomingData[0] == '0' &&
-            incomingData[1] == '0')
+            incomingData[1] == '0' &&
+            incomingData[2] == '0' &&
+            incomingData[3] == '0' &&
+            incomingData[4] == '0' &&
+            incomingData[5] == '0' &&
+            incomingData[6] == '0' &&
+            incomingData[7] == '0' &&
+            incomingData[8] == '0' &&
+            incomingData[9] == '0')
         {
             Serial.println("Elbow STOP!");
             stopElbow();
