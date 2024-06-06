5ff734d05fc7ff48732f0a21bcd45b4563c9f8a1
DSikes <pockybum522@gmail.com>
2024-06-06 10:06:29 -0400

    Set up for pro mini, added led lighting on activity for troubleshooting

--- arduino-src/joysticks-controller-podium-board/src/main.cpp
+++ arduino-src/joysticks-controller-podium-board/src/main.cpp
@@ -1,19 +1,19 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-constexpr int PIN_LEFTSTICK_DIRECTION_UP =     2;
-constexpr int PIN_LEFTSTICK_DIRECTION_RIGHT =  3;
-constexpr int PIN_LEFTSTICK_DIRECTION_DOWN =   4;
-constexpr int PIN_LEFTSTICK_DIRECTION_LEFT =   5;
-constexpr int PIN_LEFTSTICK_DIRECTION_BUTTON = 6;
+constexpr int PIN_LEFTSTICK_DIRECTION_UP =     2;   // ORANGE on CAT6
+constexpr int PIN_LEFTSTICK_DIRECTION_RIGHT =  3;   // GRREN on CAT6
+constexpr int PIN_LEFTSTICK_DIRECTION_DOWN =   4;   // BLUE on CAT6
+constexpr int PIN_LEFTSTICK_DIRECTION_LEFT =   5;   // BROWN on CAT6
+constexpr int PIN_LEFTSTICK_DIRECTION_BUTTON = 6;   // WHITE/GREEN striped on CAT6
 
-constexpr int PIN_RIGHTSTICK_DIRECTION_UP =     7;
-constexpr int PIN_RIGHTSTICK_DIRECTION_RIGHT =  8;
-constexpr int PIN_RIGHTSTICK_DIRECTION_DOWN =   9;
-constexpr int PIN_RIGHTSTICK_DIRECTION_LEFT =   10;
-constexpr int PIN_RIGHTSTICK_DIRECTION_BUTTON = 11;
+constexpr int PIN_RIGHTSTICK_DIRECTION_UP =     7;  // ORANGE on CAT6
+constexpr int PIN_RIGHTSTICK_DIRECTION_RIGHT =  8;  // GRREN on CAT6
+constexpr int PIN_RIGHTSTICK_DIRECTION_DOWN =   9;  // BLUE on CAT6
+constexpr int PIN_RIGHTSTICK_DIRECTION_LEFT =   10; // BROWN on CAT6
+constexpr int PIN_RIGHTSTICK_DIRECTION_BUTTON = 11; // WHITE/GREEN striped on CAT6
 
-SoftwareSerial mainBoardSerial(13, 12); // RX, TX
+SoftwareSerial mainBoardSerial(A0, 12); // RX, TX
 
 void setup()
 {
@@ -32,7 +32,12 @@ void setup()
     pinMode(PIN_RIGHTSTICK_DIRECTION_DOWN, INPUT_PULLUP);
     pinMode(PIN_RIGHTSTICK_DIRECTION_LEFT, INPUT_PULLUP);
     pinMode(PIN_RIGHTSTICK_DIRECTION_BUTTON, INPUT_PULLUP);
+
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
 }
+
+void turnOnLedIfAnyJoystickActivity(bool leftStickDirectionUp, bool leftStickDirectionRight, bool leftStickDirectionDown, bool leftStickDirectionLeft, bool leftStickButton, bool rightStickDirectionUp, bool rightStickDirectionRight, bool rightStickDirectionDown, bool rightStickDirectionLeft, bool rightStickButton);
 
 void loop()
 {
@@ -40,27 +45,38 @@ void loop()
     const bool leftStickDirectionRight = !digitalRead(PIN_LEFTSTICK_DIRECTION_RIGHT);
     const bool leftStickDirectionDown = !digitalRead(PIN_LEFTSTICK_DIRECTION_DOWN);
     const bool leftStickDirectionLeft = !digitalRead(PIN_LEFTSTICK_DIRECTION_LEFT);
-    const bool leftStickDirectionButton = !digitalRead(PIN_LEFTSTICK_DIRECTION_BUTTON);
+    const bool leftStickButton = !digitalRead(PIN_LEFTSTICK_DIRECTION_BUTTON);
 
     const bool rightStickDirectionUp = !digitalRead(PIN_RIGHTSTICK_DIRECTION_UP);
     const bool rightStickDirectionRight = !digitalRead(PIN_RIGHTSTICK_DIRECTION_RIGHT);
     const bool rightStickDirectionDown = !digitalRead(PIN_RIGHTSTICK_DIRECTION_DOWN);
     const bool rightStickDirectionLeft = !digitalRead(PIN_RIGHTSTICK_DIRECTION_LEFT);
-    const bool rightStickDirectionButton = !digitalRead(PIN_RIGHTSTICK_DIRECTION_BUTTON);
+    const bool rightStickButton = !digitalRead(PIN_RIGHTSTICK_DIRECTION_BUTTON);
+
+    turnOnLedIfAnyJoystickActivity(leftStickDirectionUp,
+        leftStickDirectionRight,
+        leftStickDirectionDown,
+        leftStickDirectionLeft,
+        leftStickButton,
+        rightStickDirectionUp,
+        rightStickDirectionRight,
+        rightStickDirectionDown,
+        rightStickDirectionLeft,
+        rightStickButton);
 
     // Left stick
     mainBoardSerial.print(leftStickDirectionUp);
     mainBoardSerial.print(leftStickDirectionRight);
     mainBoardSerial.print(leftStickDirectionDown);
     mainBoardSerial.print(leftStickDirectionLeft);
-    mainBoardSerial.print(leftStickDirectionButton);
+    mainBoardSerial.print(leftStickButton);
 
     // Right stick
     mainBoardSerial.print(rightStickDirectionUp);
     mainBoardSerial.print(rightStickDirectionRight);
     mainBoardSerial.print(rightStickDirectionDown);
     mainBoardSerial.print(rightStickDirectionLeft);
-    mainBoardSerial.print(rightStickDirectionButton);
+    mainBoardSerial.print(rightStickButton);
 
     mainBoardSerial.println();
 
@@ -79,4 +95,19 @@ void loop()
     // Serial.print(rightStickDirectionButton);
     //
     // Serial.println();
+}
+
+void turnOnLedIfAnyJoystickActivity(bool leftStickDirectionUp, bool leftStickDirectionRight, bool leftStickDirectionDown, bool leftStickDirectionLeft, bool leftStickButton,
+                                   bool rightStickDirectionUp, bool rightStickDirectionRight, bool rightStickDirectionDown, bool rightStickDirectionLeft, bool rightStickButton)
+{
+
+    if (leftStickDirectionUp || leftStickDirectionRight || leftStickDirectionDown || leftStickDirectionLeft || leftStickButton ||
+        rightStickDirectionUp || rightStickDirectionRight || rightStickDirectionDown || rightStickDirectionLeft || rightStickButton)
+    {
+        digitalWrite(LED_BUILTIN, HIGH);
+    }
+    else
+    {
+        digitalWrite(LED_BUILTIN, LOW);
+    }
 }
