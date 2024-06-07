ee1620a59fa4d57717c61d105f823bb36a738aeb
DSikes <pockybum522@gmail.com>
2024-06-07 14:44:42 -0400

    Everything working all the way through component chain except for shoulder rotation

--- arduino-src/main-board-arm-controller/src/main.cpp
+++ arduino-src/main-board-arm-controller/src/main.cpp
@@ -1,12 +1,31 @@
 #include <Arduino.h>
 #include <HardwareSerial.h>
+#include "../.pio/libdeps/esp32-s3-devkitm-1/elapsedMillis/elapsedMillis.h"
 
-constexpr int PIN_ELBOW_UP =   17;
-constexpr int PIN_ELBOW_DOWN = 16;
+// DO NOT USE GPIO 46 according to safe ESP32-S3 pins chart
+constexpr int PIN_ELBOW_UP =                            36;
+constexpr int PIN_ELBOW_DOWN =                          48;
+
+constexpr int PIN_SHOULDER_UP =                         21;
+constexpr int PIN_SHOULDER_DOWN =                       47;
+
+constexpr int PIN_SHOULDER_ROTATE_CLOCKWISE =           37;
+constexpr int PIN_SHOULDER_ROTATE_COUNTERCLOCKWISE =    38;
+
+constexpr int PIN_WRIST_ROTATE_CLOCKWISE =              16;
+constexpr int PIN_WRIST_ROTATE_COUNTERCLOCKWISE =       15;
+
+constexpr int PIN_HAND_CLOSE =                          1;
+constexpr int PIN_HAND_OPEN =                           2;
 
 HardwareSerial SerialPort2 (2);
 
 std::string incomingData = "";
+std::string lastIncomingData = "";
+
+
+elapsedMillis sinceJoysticksStateChange;
+
 int incomingDataLength = 0;
 bool serialLineStarted = false;
 
@@ -17,15 +36,37 @@ void setup()
     pinMode(PIN_ELBOW_UP, OUTPUT);
     pinMode(PIN_ELBOW_DOWN, OUTPUT);
 
+    pinMode(PIN_SHOULDER_UP, OUTPUT);
+    pinMode(PIN_SHOULDER_DOWN, OUTPUT);
+
+    pinMode(PIN_SHOULDER_ROTATE_CLOCKWISE, OUTPUT);
+    pinMode(PIN_SHOULDER_ROTATE_COUNTERCLOCKWISE, OUTPUT);
+
+    pinMode(PIN_WRIST_ROTATE_CLOCKWISE, OUTPUT);
+    pinMode(PIN_WRIST_ROTATE_COUNTERCLOCKWISE, OUTPUT);
+
+    pinMode(PIN_HAND_CLOSE, OUTPUT);
+    pinMode(PIN_HAND_OPEN, OUTPUT);
+
     digitalWrite(PIN_ELBOW_UP, LOW);
     digitalWrite(PIN_ELBOW_DOWN, LOW);
+
+    digitalWrite(PIN_SHOULDER_UP, LOW);
+    digitalWrite(PIN_SHOULDER_DOWN, LOW);
+
+    digitalWrite(PIN_SHOULDER_ROTATE_CLOCKWISE, LOW);
+    digitalWrite(PIN_SHOULDER_ROTATE_COUNTERCLOCKWISE, LOW);
+
+    digitalWrite(PIN_WRIST_ROTATE_CLOCKWISE, LOW);
+    digitalWrite(PIN_WRIST_ROTATE_COUNTERCLOCKWISE, LOW);
+
+    digitalWrite(PIN_HAND_CLOSE, LOW);
+    digitalWrite(PIN_HAND_OPEN, LOW);
 
     SerialPort2.begin(9600, SERIAL_8N1, 5, 4);
 }
 
-void moveElbowUp();
-void moveElbowDown();
-void stopElbow();
+void moveRobotArm();
 
 void loop()
 {
@@ -51,71 +92,16 @@ void loop()
         Serial.print("incoming line was: ");
         Serial.println(incomingData.c_str());
 
-        // Left stick
-        if (incomingData[0] == '1')
+        if (sinceJoysticksStateChange > 400 &&
+            incomingData != lastIncomingData)
         {
-            Serial.println("Left stick move: UP!");
-        }
+            sinceJoysticksStateChange = 0;
 
-        if (incomingData[1] == '1')
-        {
-            Serial.println("Left stick move: RIGHT!");
-        }
+            moveRobotArm();
 
-        if (incomingData[2] == '1')
-        {
-            Serial.println("Left stick move: DOWN!");
-        }
+            lastIncomingData = incomingData;
 
-        if (incomingData[3] == '1')
-        {
-            Serial.println("Left stick move: LEFT!");
-        }
-
-        if (incomingData[4] == '1')
-        {
-            Serial.println("Left stick: BUTTON!");
-        }
-
-        // Right stick
-        if (incomingData[5] == '1')
-        {
-            Serial.println("Right stick move: UP!");
-        }
-
-        if (incomingData[6] == '1')
-        {
-            Serial.println("Right stick move: RIGHT!");
-        }
-
-        if (incomingData[7] == '1')
-        {
-            Serial.println("Right stick move: DOWN!");
-        }
-
-        if (incomingData[8] == '1')
-        {
-            Serial.println("Right stick move: LEFT!");
-        }
-
-        if (incomingData[9] == '1')
-        {
-            Serial.println("Right stick: BUTTON!");
-        }
-
-        if (incomingData[0] == '0' &&
-            incomingData[1] == '0' &&
-            incomingData[2] == '0' &&
-            incomingData[3] == '0' &&
-            incomingData[4] == '0' &&
-            incomingData[5] == '0' &&
-            incomingData[6] == '0' &&
-            incomingData[7] == '0' &&
-            incomingData[8] == '0' &&
-            incomingData[9] == '0')
-        {
-            Serial.println("Elbow STOP!");
-            stopElbow();
+            Serial.println("Moving robot arm since delay was elapsed");
         }
 
         incomingData = "";
@@ -124,16 +110,171 @@ void loop()
 
 void moveElbowUp()
 {
+    digitalWrite(PIN_ELBOW_DOWN, LOW);
     digitalWrite(PIN_ELBOW_UP, HIGH);
+    Serial.println("Moving elbow: UP!");
 }
 
 void moveElbowDown()
 {
+    digitalWrite(PIN_ELBOW_UP, LOW);
     digitalWrite(PIN_ELBOW_DOWN, HIGH);
+    Serial.println("Moving elbow: DOWN!");
 }
 
 void stopElbow()
 {
     digitalWrite(PIN_ELBOW_DOWN, LOW);
     digitalWrite(PIN_ELBOW_UP, LOW);
+    Serial.println("Elbow: STOP!");
+}
+
+void moveShoulderUp()
+{
+    digitalWrite(PIN_SHOULDER_DOWN, LOW);
+    digitalWrite(PIN_SHOULDER_UP, HIGH);
+    Serial.println("Moving shoulder: UP!");
+}
+
+void moveShoulderDown()
+{
+    digitalWrite(PIN_SHOULDER_UP, LOW);
+    digitalWrite(PIN_SHOULDER_DOWN, HIGH);
+    Serial.println("Moving shoulder: DOWN!");
+}
+
+void stopShoulder()
+{
+    digitalWrite(PIN_SHOULDER_DOWN, LOW);
+    digitalWrite(PIN_SHOULDER_UP, LOW);
+    Serial.println("Shoulder: STOP!");
+}
+
+void openHand()
+{
+    digitalWrite(PIN_HAND_CLOSE, LOW);
+    digitalWrite(PIN_HAND_OPEN, HIGH);
+    Serial.println("Moving hand: OPEN!");
+}
+
+void closeHand()
+{
+    digitalWrite(PIN_HAND_OPEN, LOW);
+    digitalWrite(PIN_HAND_CLOSE, HIGH);
+    Serial.println("Moving hand: CLOSE!");
+}
+
+void stopHand()
+{
+    digitalWrite(PIN_HAND_OPEN, LOW);
+    digitalWrite(PIN_HAND_CLOSE, LOW);
+    Serial.println("Hand: STOP!");
+}
+
+void wristClockwise()
+{
+    digitalWrite(PIN_WRIST_ROTATE_COUNTERCLOCKWISE, LOW);
+    digitalWrite(PIN_WRIST_ROTATE_CLOCKWISE, HIGH);
+    Serial.println("Moving hand: OPEN!");
+}
+
+void wristCounterclockwise()
+{
+    digitalWrite(PIN_WRIST_ROTATE_CLOCKWISE, LOW);
+    digitalWrite(PIN_WRIST_ROTATE_COUNTERCLOCKWISE, HIGH);
+    Serial.println("Moving hand: CLOSE!");
+}
+
+void stopWristRotation()
+{
+    digitalWrite(PIN_WRIST_ROTATE_COUNTERCLOCKWISE, LOW);
+    digitalWrite(PIN_WRIST_ROTATE_CLOCKWISE, LOW);
+    Serial.println("Hand: STOP!");
+}
+
+void moveRobotArm()
+{
+    // Left stick
+    if (incomingData[0] == '1')
+    {
+        Serial.println("Left stick move: UP!");
+        moveShoulderUp();
+    }
+
+    if (incomingData[1] == '1')
+    {
+        Serial.println("Left stick move: RIGHT!");
+    }
+
+    if (incomingData[2] == '1')
+    {
+        Serial.println("Left stick move: DOWN!");
+        moveShoulderDown();
+    }
+
+    if (incomingData[0] == '0' &&
+        incomingData[2] == '0')
+    {
+        stopShoulder();
+    }
+
+    if (incomingData[3] == '1')
+    {
+        Serial.println("Left stick move: LEFT!");
+    }
+
+    if (incomingData[4] == '1')
+    {
+        Serial.println("Left stick: BUTTON!");
+        openHand();
+    }
+
+    // Right stick
+    if (incomingData[5] == '1')
+    {
+        Serial.println("Right stick move: UP!");
+        moveElbowUp();
+    }
+
+    if (incomingData[6] == '1')
+    {
+        Serial.println("Right stick move: RIGHT!");
+        wristClockwise();
+    }
+
+    if (incomingData[7] == '1')
+    {
+        Serial.println("Right stick move: DOWN!");
+        moveElbowDown();
+    }
+
+    if (incomingData[5] == '0' &&
+        incomingData[7] == '0')
+    {
+        stopElbow();
+    }
+
+    if (incomingData[8] == '1')
+    {
+        Serial.println("Right stick move: LEFT!");
+        wristCounterclockwise();
+    }
+
+    if (incomingData[6] == '0' &&
+        incomingData[8] == '0')
+    {
+        stopWristRotation();
+    }
+
+    if (incomingData[9] == '1')
+    {
+        Serial.println("Right stick: BUTTON!");
+        closeHand();
+    }
+
+    if (incomingData[4] == '0' &&
+        incomingData[9] == '0')
+    {
+        stopHand();
+    }
 }
